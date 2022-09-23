04dffff4f75b215a7fdbf6c389d7a9145d3b3ead
Maria Vitoria Pereira <86569959+mvitoriapereirac@users.noreply.github.com>
2022-09-23 11:24:56 -0300

    ajustes pre apresentacao

---
+++ ChallengeParis/Models/EmAlta.swift
@@ -0,0 +1,8 @@
+//
+//  EmAlta.swift
+//  ChallengeParis
+//
+//  Created by mvitoriapereirac on 22/09/22.
+//
+
+import Foundation
---
+++ ChallengeParis/Views/CardsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CardsCollectionViewCell.swift
+//  ChallengeParis
+//
+//  Created by mvitoriapereirac on 22/09/22.
+//
+
+import UIKit
+
+class CardsCollectionViewCell: UICollectionViewCell {
+    
+}
